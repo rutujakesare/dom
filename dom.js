adfaf6266bef58eb2d1de1481a990da5cd29d647
rutujakesare <rutujakesare38@gmail.com>
2023-07-06 01:54:08 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,33 @@
+//EXAMINE THE DOCUMENT OBJECT//
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms);
+console.log(document.links);
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].style.fontWeight='bold';
+titles[0].style.color='green'; 
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor = 'green';
+
+for(var i =0; i<items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
